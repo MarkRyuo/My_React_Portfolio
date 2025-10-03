880223133615185023286bbd414034a7c13c5ad1
MarkRyuo <malupajhonmark3035@gmail.com>
2025-10-03 19:37:20 +0800

    Subject: First Commit

---
+++ src/components/header.jsx
@@ -0,0 +1,17 @@
+
+
+function Header() {
+
+    return (
+
+        <Header>
+            
+            <nav className="">
+                <h1>This is nav inside of header</h1>
+            </nav>
+        </Header>
+
+    )
+}
+
+export default Header ;
